8955f83ad93e5394250225aece4819733b263f34
Mario Izquierdo <marioizquierdopujante@gmail.com>
2025-05-06 14:33:49 +0200

    mas o menos WallRunning

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,20 @@ public class PlayerMovement : MonoBehaviour
 
 
     [Header("Movement")]
-    public float moveSpeed;
+    private float moveSpeed;
+
+    public float walkSpeed;
+    public float sprintSpeed;
     public Transform orientation;
      public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
+
+    public float fallMultiplier;
+
+    public float jumpFallMultiplier;
+
+    public float wallrunSpeed;
 
     [Header("Ground Check")]
 
@@ -19,10 +28,14 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask whatIsGround;
     public bool grounded;
 
+    
     public float groundDrag;
+      public float airDrag;
 
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+
+    public KeyCode sprintKey = KeyCode.LeftShift;
    
 
     bool readyToJump;
@@ -32,6 +45,20 @@ public class PlayerMovement : MonoBehaviour
     Vector3 moveDirection;
 
     Rigidbody rb;
+
+    public MovementState state; 
+
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air,
+
+        wallrunning,
+    }
+
+    public bool wallrunning;
 
     void Start()
     {
@@ -43,24 +70,45 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+{
+    MyInput(); // Get input from the player
+
+    // Check if the player is grounded
+    grounded = Physics.Raycast(transform.position, Vector3.down, (playerHeight) + 0.3f, whatIsGround);
+
+    if (grounded)
     {
-        MyInput(); // Get input from the player
-      grounded = Physics.Raycast(transform.position, Vector3.down, (playerHeight / 2) + 0.3f, whatIsGround);
-        if (grounded)
-        {
-            rb.drag = groundDrag; // Apply drag when grounded
-        }
-        else
-        {
-            rb.drag = 0; // No drag when in the air
-        }
-        SpeedControl(); // Control the speed of the player
+        rb.drag = groundDrag; // Apply drag when grounded
     }
+    else
+    {
+        rb.drag = airDrag; // Apply air drag when in the air
+    }
+
+    SpeedControl(); // Control the speed of the player
+
+    // Apply additional gravity if the spacebar is released while in the air
+    if (!grounded && Input.GetKeyUp(jumpKey))
+    {
+        rb.velocity += Vector3.up * Physics.gravity.y * (jumpFallMultiplier - 1) * Time.deltaTime;
+    }
+
+    // Apply fall multiplier when falling
+    if (rb.velocity.y < 0)
+    {
+        rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+    }
+
+    StateHandler(); // Handle the player's state (walking, sprinting, air)
+}
 
     void FixedUpdate()
     {
         MovePlayer();
         Debug.Log($"Grounded: {grounded}");
+        
+
+
     }
 
     private void MyInput()
@@ -90,6 +138,8 @@ public class PlayerMovement : MonoBehaviour
         Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
         rb.velocity = Vector3.Lerp(flatVelocity, Vector3.zero, Time.deltaTime * 10f) + new Vector3(0f, rb.velocity.y, 0f);
     }
+    
+
 }
 private void SpeedControl(){
     Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z); // Get the horizontal velocity
@@ -117,9 +167,37 @@ private void SpeedControl(){
 
 private void Jump(){
 
-    rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); // Reset the vertical velocity
+    rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); // Reset the vertical velocity to 0 before jumping
     rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); 
 
 
 }   
+
+
+private void StateHandler(){
+    // Mode - Sprint
+    if(grounded && Input.GetKey(sprintKey)) 
+    {
+        state = MovementState.sprinting; // Set the state to sprinting
+        moveSpeed = sprintSpeed; // Set the movement speed to sprint speed  
+          }
+
+    // Mode - Walk
+    else if (grounded){
+     state = MovementState.walking; // Set the state to walking
+        moveSpeed = walkSpeed; // Set the movement speed to walk speed
+    } 
+
+    // Mode - Air
+    else {
+        state = MovementState.air; // Set the state to air    
+    }
+
+    // Mode -Wallruning
+    if (wallrunning) {
+        state = MovementState.wallrunning; // Set the state to wallrunning
+        moveSpeed = wallrunSpeed; // Set the movement speed to wallrun speed    
+    }
 }
+}
+
